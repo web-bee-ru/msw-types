ee44ce98c21365f61a2cf30a0d652df02d5a2494
Denwa799 <denwa799@yandex.ru>
2024-01-10 15:00:22 +0300

    DEVTASKS-44. Делает типизацию под http msw

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from "./TypedRest";
+import { HttpResponse, http } from "msw";
+import { TypedHttp } from "./TypedHttp";
+import { TestApi } from "./data";
+
+const typedHttp = new TypedHttp<TestApi>(http);
+
+typedHttp.get("/test1", async ({ request, params }) => {
+  return HttpResponse.text("test");
+});
