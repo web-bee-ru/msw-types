69c593781e5d45106502b7a37162ffccdab4e963
Denwa799 <denwa799@yandex.ru>
2024-01-10 16:12:10 +0300

    DEVTASKS-44: выносит example. Меняет команды сборки. Удаляет TypedRest

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { HttpResponse, http } from "msw";
-import { TypedHttp } from "./TypedHttp";
-import { TestApi } from "./data";
-
-const typedHttp = new TypedHttp<TestApi>(http);
-
-typedHttp.get("/test1", async ({ request, params }) => {
-  return HttpResponse.text("test");
-});
+export * from "./TypedHttp";
